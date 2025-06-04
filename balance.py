14967519cfa5bdccef35bd29eca25eb69d31efcf
Pololelka <pololelka@gmail.com>
2025-06-04 18:13:20 +0300

    Раписаны категории, все работает

--- balance.py
+++ balance.py
@@ -5,7 +5,7 @@ FILE_BALANCE = "data/balance.json"
 
 def add_to_balance(id_catogory, amount):
     balance = load_file(FILE_BALANCE)
-    balance[0]["balance"] -= amount if id_catogory != 0 else +amount
+    balance[0]["balance"] += -amount if id_catogory != 0 else amount
     save_file(FILE_BALANCE, balance)
 
 
